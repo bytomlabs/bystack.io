065dec2b4a216af4c72e8a64fcc94ab70376e03f
qiaoyu <rainoy.me@gmail.com>
2019-06-04 18:23:39 +0800

    bystack node

---
+++ english/assets/js/contact.js
@@ -0,0 +1,27 @@
+$(function(){
+  $('#contact_submit').click(function(event){
+    event.preventDefault();
+    console.log('submit');
+    var email = $('#xs_contact_email').val();
+    var name = $('#xs_contact_name').val();
+    var msg = $('#x_contact_massage').val() || 'bystack';
+    if(!email.trim() || !name.trim()){
+      layer.msg('请填写表单后提交');
+      return;
+    }
+
+    const url = '//mc.us20.list-manage.com/subscribe/form-post-json?u=05d404619cf8ee88ee605c21c&id=fccb1911a5&popup=true';
+    $.get(url + '&EMAIL=' + email + '&NAME=' + name + '&MSG' + msg).then(function(data){
+      if(data.result === 'success'){
+        layer.msg('提交成功');
+        $('#xs_contact_email').val('');
+        $('#xs_contact_name').val('');
+        $('#x_contact_massage').val('');
+      } else if(data.result === 'error'){
+        layer.msg('请不要重复提交');
+      }
+    });
+
+
+  })
+});
