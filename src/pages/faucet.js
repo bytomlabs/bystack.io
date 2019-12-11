53e503bd79d326c22c0d5c94f64fd20804410ddc
qiaoyu <rainoy.me@gmail.com>
2019-12-11 17:22:00 +0800

    update

--- src/pages/faucet.js
+++ src/pages/faucet.js
@@ -4,7 +4,7 @@ import { FormattedMessage as Msg } from 'gatsby-plugin-intl';
 import _ajax from 'axios';
 
 import SEO from '../components/Seo';
-import { PageHeader, Avatar, Input, Select, Button } from 'antd';
+import { PageHeader, Avatar, Input, Select, message } from 'antd';
 
 const apiHost = 'http://52.82.24.162:5000/api/v1/get_testnet_coins';
 
@@ -14,62 +14,117 @@ const Cont = css.div`
 const Title = css.div`
   font-size: 14px;
   line-height: 32px;
+  color: #000;
+`;
+const Address = css.div`
+  padding-top: 30px;
+  p{
+    word-wrap: break-word;
+    color: #999;
+  }
+`;
+const Button = css.div`
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  font-size: 16px;
+  border-radius: 4px;
+  color: #000;
+  background: rgba(228, 176, 90);
+  cursor: pointer;
+  &:active {
+    opacity: .8;
+  }
 `;
 const W = css.div``;
-
+let timer = null;
 const Home = () => {
-  const [curAsset, setCurAsset] = useState('btm');
+  const [curAsset, setCurAsset] = useState('usdt');
   const [curAddress, setAddress] = useState('');
+
+  useEffect(() => {
+    try {
+      if(window) {
+        document.addEventListener('chromeBytomLoaded', bytomExtension => {
+          getAccountInfo();
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  
+
+  const getAccountInfo = async () => {
+    try {
+      const account = await window.bytom.enable();
+      console.log(account.address)
+      if(account.address) {
+        setAddress(account.address);
+        timer && clearTimeout(timer);
+      }
+    } catch (error) {
+      console.log(error);
+      timer = setTimeout(() => {
+        getAccountInfo();
+      }, 1000);
+    }
+  }
 
   const handleSubmit = () => {
     _ajax
-      .post(apiHost, {asset: curAsset, address: curAddress})
+      .post(apiHost, {asset: curAsset, address: 'tp1qjchcu4qlny2xq508mp4a2ekptk5dkugdlseekc'})
       .then(res => {
-        console.log(res.data)
+        if(res.data.tx_id) {
+          message.success('领取成功 Success');
+        }
       })
       .catch(err => {
         console.log(err);
+        message.error('领取失败 Fail');
       })
   }
 
   return  (
     <>
       <SEO
-        title="比原链水龙头" 
+        title="MOV水龙头" 
       />
       <PageHeader
         style={{ borderBottom: '1px solid rgb(235, 237, 240)', }}
-        title={<><Avatar style={{ color: '#fff', backgroundColor: '#87d068' }}>BTM</Avatar> 比原链水龙头</>}
-        subTitle="Bytom Faucet"
+        title={<span style={{ color: 'rgb(228, 176, 90)' }}>MOV水龙头</span>}
+        subTitle={<p style={{textAlign: 'right', margin: 0}}>MOV Faucets</p>}
       ></PageHeader>
       <Cont>
-        <Title>选择资产</Title>
+        <Title>选择资产 <span style={{color: 'rgba(0, 0, 0, 0.45)'}}>Select Asset</span></Title>
         <Select 
           defaultValue={curAsset} 
           onChange={setCurAsset}
+          size="large"
           style={{ width: '100%', margin: '10px 0 25px' }}
         >
-          <Select.Option value="btm">BTM</Select.Option>
+          <Select.Option value="usdt">USDT</Select.Option>
           <Select.Option value="btc">BTC</Select.Option>
           <Select.Option value="eth">ETH</Select.Option>
-          <Select.Option value="usdt">USDT</Select.Option>
+          <Select.Option value="btm">BTM</Select.Option>
         </Select>
-        <Title>填写地址</Title>
-        <Input
-          allowClear
+        {/* <Input.TextArea
+          // allowClear
           style={{ width: '100%', margin: '10px 0 25px' }}
           value={curAddress}
           onChange={e => setAddress(e.target.value)}
           placeholder="请输入钱包地址"
-        />
+          readOnly
+        /> */}
         <Button 
-          block 
-          ghost 
-          size="large" 
-          type="primary"
-          style={{ marginTop: 20 }}
           onClick={handleSubmit}
-        >领取资产</Button>
+        >领取 Get</Button>
+        
+        <Address>
+          <Title>我的地址 <span style={{color: 'rgba(0, 0, 0, 0.45)'}}>Address</span>: </Title>
+          <p>{curAddress}</p>
+        </Address>
       </Cont>
     </>
   );
